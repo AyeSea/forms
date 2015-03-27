09a559cba9d4a7c3f879a649ebb36042e0077428
Andrew Chung <andrew.chung159@gmail.com>
2015-03-27 18:09:20 -0400

    Set up back end (routes, User controller & views, User model validations)

--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #create ONLY the routes for GET /users/new and POST /users
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
